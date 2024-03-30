a250a9090c3919233d671c5335ae16d14874ccef
gdcurs <33995878+gdcurs@users.noreply.github.com>
2024-03-31 02:14:18 +0800

    Update generate-docker-mem-check.sh

--- generate-docker-mem-check.sh
+++ generate-docker-mem-check.sh
@@ -13,8 +13,8 @@ cat << EOF > /root/docker-mem-check.sh
 # 自定义语句1
 custom_command1="$custom_command1"
 
-# 延迟5分钟后删除所有容器
-# sleep 2m
+# 延迟2分钟后删除所有容器
+sleep 2m
 if [ "\$(docker ps -aq)" ]; then
     # 存在容器,删除所有容器
     echo "发现存在容器,正在删除所有容器..."
@@ -23,15 +23,15 @@ if [ "\$(docker ps -aq)" ]; then
 else
     echo "当前没有运行的容器。"
 fi
-#sleep 1m
+sleep 1m
 # 执行自定义语句1
 eval \$custom_command1
 
 while true
 do
-    # 延迟5分钟
+    # 延迟3分钟
     echo "开始监控。"
-    sleep 1m
+    sleep 3m
 
     # 获取所有正在运行的Docker容器ID
     container_ids=\$(docker ps --format "{{.ID}}")
