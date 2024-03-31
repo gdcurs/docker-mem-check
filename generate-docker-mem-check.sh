6e225ce8fe9d11544243414130a92d1adeb6c953
gdcurs <33995878+gdcurs@users.noreply.github.com>
2024-03-31 12:51:36 +0800

    Update generate-docker-mem-check.sh

--- generate-docker-mem-check.sh
+++ generate-docker-mem-check.sh
@@ -45,7 +45,7 @@ do
         #echo echo "容器 \$container_id 的使用内存大小为: \$mem_usage+\$mem_usage_unit"
         # 如果内存使用量低于10MB,则删除所有容器并执行自定义语句2
         if (( \$(echo "\$mem_usage < 10" | bc -l) )) || [ "\$mem_usage_unit" = "KiB" ]; then
-            sleep 5m
+            sleep 15m
             # 获取容器的内存使用情况(以MB为单位)
             mem_usage=\$(docker stats --no-stream --format "{{.MemUsage}}" \$container_id | awk '{print \$1}' | tr -d '[:alpha:]')
             mem_usage_unit=\$(docker stats --no-stream --format "{{.MemUsage}}" \$container_id | awk '{print \$1}' | tr -d '[:digit:]')
